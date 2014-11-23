c3ec673d14e501f9c8cf602a318cdca66e34919d
Mike OShea <mike@limpidfox.com>
2014-11-23 00:25:08 -0800

    New change

--- Program.cs
+++ Program.cs
@@ -6,7 +6,8 @@ namespace monoconsole
 	{
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Hello World!");
+			Console.WriteLine ("Hello World!!!");
+			Console.WriteLine ("This is modified");
 		}
 	}
 }
